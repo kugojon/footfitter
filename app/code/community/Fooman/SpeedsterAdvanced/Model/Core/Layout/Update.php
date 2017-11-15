889895df2f97feaa5be4d0c80044bde37aba1404
TrungNguyen <jon@kugo.com>
2017-11-14 21:11:58 -0500

    v254-126 fix bug Broken Checkout from Gift Card

--- app/code/community/Fooman/SpeedsterAdvanced/Model/Core/Layout/Update.php
+++ app/code/community/Fooman/SpeedsterAdvanced/Model/Core/Layout/Update.php
@@ -1,6 +1,6 @@
 <?php
 
-class Fooman_SpeedsterAdvanced_Model_Core_Layout_Update extends Mage_Core_Model_Layout_Update
+class Fooman_SpeedsterAdvanced_Model_Core_Layout_Update extends AnattaDesign_MultipleHandles_Model_Core_Layout_Update
 {
 
     public function resetPackageLayout()
